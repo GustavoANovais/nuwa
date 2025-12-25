b89f498908ed9ccf819b839d18510c8aaef3fd32
GustavoANovais <149081188+GustavoANovais@users.noreply.github.com>
2025-12-25 17:50:56 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const verMaisBtn = document.getElementById("verMaisBtn");
-  const colecao = document.getElementById("colecao");
-
-  verMaisBtn.addEventListener("click", () => {
-    colecao.scrollIntoView({ behavior: "smooth" });
-  });
-});
