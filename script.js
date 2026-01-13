cfde531fabcb4f0ef405420c17139fdfe3372eb4
GustavoANovais <149081188+GustavoANovais@users.noreply.github.com>
2026-01-13 12:30:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,10 @@
-//document.addEventListener("DOMContentLoaded", () => {
-  //const verMaisBtn = document.getElementById("verMaisBtn");
-  //const colecao = document.getElementById("colecao");
+document.querySelectorAll('nav a').forEach(link => {
+  const linkPath = new URL(link.href, window.location.origin).pathname;
+  const currentPath = window.location.pathname;
 
-  //verMaisBtn.addEventListener("click", () => {
-   // colecao.scrollIntoView({ behavior: "smooth" });
-//  });
-//});
-
- // JavaScript para scroll suave nos links de navegação
-        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-            anchor.addEventListener('click', function (e) {
-                e.preventDefault();
-                const targetId = this.getAttribute('href').substring(1);
-                const targetElement = document.getElementById(targetId);
-                if (targetElement) {
-                    targetElement.scrollIntoView({
-                        behavior: 'smooth'
-                    });
-                }
-            });
-        });
-
-        let slideIndex = 0;
-        showSlides();
-
-        function showSlides() {
-            let i;
-            let slides = document.getElementsByClassName("mySlides");
-            let dots = document.getElementsByClassName("dot");
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";  
-            }
-            slideIndex++;
-            if (slideIndex > slides.length) {slideIndex = 1}    
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-            }
-            slides[slideIndex-1].style.display = "block";  
-            dots[slideIndex-1].className += " active";
-            setTimeout(showSlides, 5500); // Change image every 2 seconds
-    }
+  if (linkPath === currentPath) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+    });
+  }
+});
